c2c62330d8a1c6c25033662b61e131c28f4f8576
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-04-25 22:06:22 +0300

    Index page initial commit

--- library/views.py
+++ library/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import *
 
 
-
-def index(request):
-    return HttpResponse('Nice!')
 # Create your views here.
+def index(request):
+    book_count = Book.objects.all().count()
+    book_instance_count = BookInstance.objects.all().count()
+    available_books_count = BookInstance.objects.filter(book_status__exact='a').count()
+
+    author_count = Author.objects.all().count()
+    context = {
+        'books': book_count,
+        'book_instances': book_instance_count,
+        'authors': author_count,
+        'available': available_books_count
+    }
+    return render(request, "index.html", context)
