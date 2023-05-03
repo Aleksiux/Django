8fe189a6862ec9c78bdd64aa706c3c993c4aa513
Aleksas Kaminskas <aleksiuxx@gmail.com>
2023-05-03 18:33:13 +0300

    Fixing views

--- library/urls.py
+++ library/urls.py
@@ -21,4 +21,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('authors/', views.authors, name='author'),
+    path('authors/<int:author_id>', views.author, name='author'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    path('books/<int:pk>', views.BookDetailView.as_view(), name='book-detail'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
+
 from .models import *
 
 
@@ -16,3 +18,32 @@ def index(request):
         'available': available_books_count
     }
     return render(request, "index.html", context)
+
+
+def authors(request):
+    authors = Author.objects.all()
+    context = {
+        'authors': authors
+    }
+    return render(request, 'authors.html', context=context)
+
+
+def author(request, author_id):
+    author = get_object_or_404(Author, pk=author_id)
+    context = {
+        'author': author
+    }
+    return render(request, 'author.html', context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    # context_object_name = 'my_book_list'
+    # def get_queryset(self):
+    #     return Book.objects.filter(title__icontains='The')
+    template_name = 'book_list.html'
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    template_name = 'book_detail.html'
